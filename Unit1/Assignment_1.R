4666c09786d891bf967ec00ccce8624b8be9eb24
sleeperbus <sleeperbus@gmail.com>
2015-03-08 23:39:25 +0000

    ...

--- Unit1/Assignment_1.R
+++ Unit1/Assignment_1.R
@@ -27,4 +27,20 @@ mvt.2007 = subset(mvt, Year==2007)
 prop.table(table(mvt.2007$Arrest))
 mvt.2012 = subset(mvt, Year==2012)
 prop.table(table(mvt.2012$Arrest))
-
+sort(table(mvt$LocationDescription), TRUE)[1:6]
+Top5 = subset(mvt, LocationDescription %in% (c(
+        "STREET", 
+        "PARKING LOT/GARAGE(NON.RESID.)", 
+        "ALLEY",
+        "GAS STATION",
+        "DRIVEWAY - RESIDENTIAL"
+        
+        )
+    ))
+Top5$Weekday = factor(Top5$Weekday)
+summary(Top5)
+nrow(Top5)
+Top5$LocationDescription = factor(Top5$LocationDescription)
+Top5$LocationDescription = factor(Top5$LocationDescription)
+table(Top5$LocationDescription, Top5$Arrest)
+table(Top5$LocationDescription, Top5$Weekday)
