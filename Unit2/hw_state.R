ed0133661cc16e8333cb82e11823a208c0f9abd1
sleeperbus <sleeperbus@gmail.com>
2015-03-21 19:36:25 +0000

    ..

--- Unit2/hw_state.R
+++ Unit2/hw_state.R
@@ -14,6 +14,11 @@ tapply(statedata$HS.Grad, statedata$state.region, max)
 boxplot(statedata$Murder ~ statedata$state.region)
 state.northeast = subset(statedata, state.region == "Northeast")
 state.northeast$state.name[which.max(state.northeast$Murder)]
-lm.lifeexp = lm(Life.Exp ~ Population + Income + Illiteracy + 
+lm.life1= lm(Life.Exp ~ Population + Income + Illiteracy + 
                     Murder + HS.Grad + Frost + Area, data=statedata)
-summary(lm.lifeexp)
+summary(lm.life1)
+plot(statedata$Income, statedata$Life.Exp)
+
+lm.life2 = lm(Life.Exp ~ Population + Murder + HS.Grad + Frost, 
+              data=statedata)
+summary(lm.life2)
