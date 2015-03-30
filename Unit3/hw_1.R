c9c9060a8b41ac327fc26db84edad8046b32ed09
sleeperbus <sleeperbus@gmail.com>
2015-03-30 08:43:03 +0000

    ..

--- Unit3/hw_1.R
+++ Unit3/hw_1.R
@@ -7,3 +7,13 @@ michael$songtitle[which(michael$Top10 == 1)]
 
 table(songs$timesignature)
 songs$songtitle[which.max(songs$tempo)]
+
+train = subset(songs, year <= 2009)
+test = subset(songs, year >= 2010)
+nonvars = c("year", "songtitle", "artistname", "songID", "artistID")
+train = train[, !(names(train) %in% nonvars)]
+test = test[, !(names(test) %in% nonvars)]
+
+# model 1
+mod1 = glm(Top10 ~ ., data=train, family=binomial)
+summary(mod1)
