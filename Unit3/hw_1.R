c8390a01c0f1fc791f54e9e4872c08bab3bb87c6
sleeperbus <sleeperbus@gmail.com>
2015-03-30 23:39:08 +0900

    home

--- Unit3/hw_1.R
+++ Unit3/hw_1.R
@@ -17,3 +17,23 @@ test = test[, !(names(test) %in% nonvars)]
 # model 1
 mod1 = glm(Top10 ~ ., data=train, family=binomial)
 summary(mod1)
+cor(train$loudness, train$energy)
+
+# model 2
+mod2 = glm(Top10 ~ . -loudness, data=train, family=binomial)
+summary(mod2)
+
+# model 3
+mod3 = glm(Top10 ~ . -energy, data=train, family=binomial)
+summary(mod3)
+
+# predict
+pred3 = predict(mod3, newdata=test, type="response")
+prop.table(table(test$Top10, pred3 > 0.45))
+library(ROCR)
+ROCRpred = prediction(pred3, test$Top10)
+ROCRperf = performance(ROCRpred, "tpr", "fpr")
+plot(ROCRperf, colorize=T, print.cutoffs.at=seq(0,1,by=0.1), text.adj=c(0, 1.7))
+
+# base model
+prop.table(table(test$Top10))
