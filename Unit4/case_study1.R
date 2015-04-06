16494f0035bc71fb83686c3ec43246b186719626
sleeperbus <sleeperbus@gmail.com>
2015-04-06 03:43:30 +0000

    ...

--- Unit4/case_study1.R
+++ Unit4/case_study1.R
@@ -1,21 +1,55 @@
 library(caTools)
 library(ROCR)
+library(caret)
 
+set.seed(3000)
 stevens = read.csv("stevens.csv")
 str(stevens)
-
-split = sample.split(stevens$Reverse, SplitRatio=0.7)
+split = sample.split(stevens$Reverse, SplitRatio=0.7) 
 train = subset(stevens, split==T)
 test = subset(stevens, split==F)
 names(train)
 
+form = Reverse ~ Circuit + Issue + Petitioner + Respondent + LowerCourt +
+        Unconst
 # logistic model
-mod.log = glm(Reverse ~ Circuit + Issue + Petitioner + Respondent +
-                  LowerCourt + Unconst, data=train, family=binomial)
-summary(mod.log)
-pred.log = predict(mod.log, newdata=test, type="response")
+log.mod = glm(form, data=train, family=binomial)
+summary(log.mod)
+log.pred = predict(log.mod, newdata=test, type="response")
 prop.table(table(test$Reverse))
-prop.table(table(test$Reverse, pred.log > 0.5))
-logROCR = prediction(pred.log, test$Reverse)
-auc = performance(logROCR, "auc")
+prop.table(table(test$Reverse, log.pred > 0.5))
+log.ROCR = prediction(log.pred, test$Reverse)  
+log.auc = performance(log.ROCR, "auc")
+log.perf = performance(log.ROCR, "tpr", "fpr")
+plot(log.perf, colorize=T, print.cutoffs.at=seq(0.1, 1, 0.1), 
+     text.adj=c(0, 1.7))
 
+# CART model
+library(rpart)
+library(rpart.plot)
+library(randomForest)
+
+tree.mod = rpart(form, data=train, method="class", minbucket=25)
+summary(tree.mod)
+prp(tree.mod)
+
+# CART Model prediction
+tree.pred = predict(tree.mod, newdata=test, type="class")
+prop.table(table(test$Reverse, tree.pred))
+
+# CART ROC Curve
+tree.roc.prob= predict(tree.mod, newdata=test)
+tree.roc.pred = prediction(tree.roc.prob[,2], test$Reverse)
+tree.roc.auc = performance(tree.roc.pred, "auc")
+tree.roc.perf = performance(tree.roc.pred, "tpr", "fpr")
+plot(tree.roc.perf, print.cutoffs.at=seq(0.1, 1, 0.1), colorize=T, 
+     text.adj=c(0, 1.7))
+
+
+# Random Forest Model
+train$Reverse = as.factor(train$Reverse)
+test$Reverse = as.factor(test$Reverse)
+forest.mod = randomForest(form, data=train, ntree=200, nodesize=25)
+forest.pred = predict(forest.mod, newdata=test)
+prop.table(table(test$Reverse, forest.pred))
+           
