07d09ab657866cda2fad02edbba7812d35cc928a
sleeperbus <sleeperbus@gmail.com>
2015-04-08 00:40:26 +0900

    ..

--- Unit4/case_study2.R
+++ Unit4/case_study2.R
@@ -8,3 +8,8 @@ split = sample.split(claims$bucket2009, SplitRatio=0.6)
 train = subset(claims, split==T)
 test = subset(claims, split==F)
 
+# base line 
+sum(diag(1, 5) * table(test$bucket2009, test$bucket2008))/nrow(test)
+penaltyMatrix  = matrix(c(0, 1, 2, 3, 4, 2, 0 , 1, 2, 3, 4, 2, 0, 1, 2, 6, 4, 2, 
+                         0, 1, 8, 6, 4, 2, 0), nrow=5, ncol=5, byrow=T)
+sum(as.matrix(table(test$bucket2009, test$bucket2008)) * penaltyMatrix)/nrow(test)
