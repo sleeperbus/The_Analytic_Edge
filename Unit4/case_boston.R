cb4f587fb040ae4f2395c027aafc47afcb03ae81
Hojoon Ji <sleeperbus@gmail.com>
2015-04-10 14:16:03 +0900

    ...

---
+++ Unit4/case_boston.R
@@ -0,0 +1,15 @@
+boston = read.csv("boston.csv")
+str(boston)
+formula = MEDV ~ LON + LAT + CRIM + ZN + INDUS + CHAS + NOX + RM +
+    AGE + DIS + RAD + TAX + PTRATIO
+
+summary(boston$MEDV)
+plot(boston$LON, boston$LAT)
+points(boston$LON[boston$MEDV > 21.2], boston$LAT[boston$MEDV > 21.2],
+       col="yellow", pch=19)
+points(boston$LON[boston$MEDV > 25], boston$LAT[boston$MEDV > 25],
+       col="purple", pch=19)
+
+boxplot(boston$MEDV ~ boston$CHAS)
+summary(boston$MEDV[boston$CHAS==0])
+IQR(boston$MEDV[boston$CHAS==0])
