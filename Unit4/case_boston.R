a9dc7b86ac0914615a15b23827b95337457dd754
Hojoon Ji <sleeperbus@gmail.com>
2015-04-13 17:56:03 +0900

    ..

--- Unit4/case_boston.R
+++ Unit4/case_boston.R
@@ -1,7 +1,12 @@
 boston = read.csv("boston.csv")
 str(boston)
 summary(boston)
-formula = MEDV ~ LON + LAT + CRIM + ZN + INDUS + CHAS + NOX + RM +
+library(caTools)
+split = sample.split(boston$MEDV, SplitRatio=0.7)
+train = subset(boston, split==T)
+test = subset(boston, split==F)
+form.geo = MEDV ~ LON + LAT
+form.all = MEDV ~ LON + LAT + CRIM + ZN + INDUS + CHAS + NOX + RM +
     AGE + DIS + RAD + TAX + PTRATIO
 
 summary(boston$MEDV)
@@ -14,3 +19,7 @@ points(boston$LON[boston$MEDV > 25], boston$LAT[boston$MEDV > 25],
 boxplot(boston$MEDV ~ boston$CHAS)
 summary(boston$MEDV[boston$CHAS==0])
 IQR(boston$MEDV[boston$CHAS==0])
+
+# Geo Linear Model
+lm.geo = lm(form.geo, data=train)
+plot(test$LON, test$LAT)
---
+++ Unit4/hw_gerber.R
@@ -0,0 +1,38 @@
+gerber = read.csv("gerber.csv")
+str(gerber)
+prop.table(table(gerber$voting))
+
+tapply(gerber$voting, gerber$hawthorne, mean)
+tapply(gerber$voting, gerber$civicduty, mean)
+tapply(gerber$voting, gerber$neighbors, mean)
+tapply(gerber$voting, gerber$self, mean)
+
+# logistic model
+log.mod = glm(voting ~ hawthorne + civicduty + neighbors + self,
+              data = gerber, family=binomial)
+summary(log.mod)
+log.pred = predict(log.mod, type="response")
+table(gerber$voting, log.pred > 0.3)
+table(gerber$voting, log.pred > 0.5)
+library(ROCR)
+log.predict = prediction(log.pred, gerber$voting)
+log.perf = performance(log.predict, "tpr", "fpr")
+log.auc= performance(log.predict, "auc")
+plot(log.perf, print.cutoffs.at=seq(0,1,0.1), colorize=T,
+     text.adj=c(0, 1.7))
+
+# CART model
+library(rpart)
+library(rpart.plot)
+cart.mod = rpart(voting ~ hawthorne + civicduty + neighbors + self,
+                 data=gerber)
+prp(cart.mod)
+
+cart.mod2 = rpart(voting ~ hawthorne + civicduty + neighbors + self,
+                 data=gerber, cp=0.0)
+prp(cart.mod2)
+
+cart.mod3 = rpart(voting ~ hawthorne + civicduty + neighbors + self + sex,
+                  data=gerber, cp=0.0)
+prp(cart.mod3)
+
