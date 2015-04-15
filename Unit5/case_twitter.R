7d32aca4671bb140d8fb2991916fef61ae8cad1a
Hojoon Ji <sleeperbus@gmail.com>
2015-04-15 12:41:27 +0900

    ..

--- Unit5/case_twitter.R
+++ Unit5/case_twitter.R
@@ -11,7 +11,7 @@ corpus = tm_map(corpus, tolower)
 corpus = tm_map(corpus, PlainTextDocument)
 corpus = tm_map(corpus, removePunctuation)
 corpus = tm_map(corpus, removeWords, c("apple", stopwords("english")))
-corpus = tm_map(corpus, stemDocument)
+corpus= tm_map(corpus, stemDocument, lazy = TRUE)
 
 frequencies = DocumentTermMatrix(corpus)
 inspect(frequencies[1000:1005, 505:515])
@@ -19,6 +19,7 @@ findFreqTerms(frequencies, lowfreq = 20)
 sparse = removeSparseTerms(frequencies, 0.995)
 
 tweetSparse = as.data.frame(as.matrix(sparse))
+colnames(tweetSparse) = make.names(colnames(tweetSparse))
 tweetSparse$Negative = tweets$Negative
 
 library(caTools)
@@ -28,6 +29,8 @@ train = subset(tweetSparse, split==T)
 test = subset(tweetSparse, split==F)
 
 # CART model
+library(rpart)
+library(rpart.plot)
 cart.mod = rpart(Negative ~ ., data=train, method="class")
 prp(cart.mod)
 cart.pred = predict(cart.mod, newdata=test, type="class")
@@ -36,4 +39,13 @@ sum(diag(1,2)*cart.table)/sum(cart.table)
 
 # base line
 300/(300+55)
+
+# random forest
+library(randomForest)
+set.seed(123)
+forest.mod = randomForest(Negative ~ ., data=train)
+forest.pred = predict(forest.mod, newdata=test)
+forest.table = table(test$Negative, forest.pred)
+forest.table
+sum(diag(1,2)*forest.table)/sum(forest.table)
 
