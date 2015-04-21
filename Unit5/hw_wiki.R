3a0ffb7ffc7ba7052daf0e582fd7434b2607367d
Hojoon Ji <sleeperbus@gmail.com>
2015-04-21 12:46:48 +0900

    ..

--- Unit5/hw_wiki.R
+++ Unit5/hw_wiki.R
@@ -3,6 +3,7 @@ str(wiki)
 wiki$Vandal = as.factor(wiki$Vandal)
 table(wiki$Vandal)
 
+library(tm)
 corpusAdded = Corpus(VectorSource(wiki$Added))
 corpusAdded = tm_map(corpusAdded, removeWords, stopwords("english"))
 corpusAdded = tm_map(corpusAdded, stemDocument)
@@ -55,5 +56,5 @@ cart.pred2 = predict(cart.mod2, newdata=wikiTest2)
 cart.table2 = table(wikiTest2$Vandal, cart.pred2[,2] > 0.5)
 sum(diag(1,2)*cart.table2)/sum(cart.table2)
 
-
-rowSums(as.matrix(dtmAdded))
+wikiWords2$NumWordsAdded = rowSums(as.matrix(dtmAdded))
+wikiWords2$NumWordsRemoved= rowSums(as.matrix(dtmRemoved))
