fa7dc579c9d1b0e488e452a7421b41b6856fcc11
Hojoon Ji <sleeperbus@gmail.com>
2015-05-06 12:50:13 +0900

    ..

--- Unit6/hw_airlines.R
+++ Unit6/hw_airlines.R
@@ -5,3 +5,27 @@ head(airlines)
 
 # normalize data
 library(caret)
+preproc = preProcess(airlines)
+airlinesNorm = predict(preproc, airlines)
+str(airlinesNorm)
+summary(airlinesNorm)
+
+# hclust
+distances = dist(airlinesNorm, method="euclidean")
+clustH = hclust(distances, method="ward.D")
+plot(clustH)
+rect.hclust(clustH, k=5)
+groupsH = cutree(clustH, k=5)
+table(groupsH)
+
+spl = split(airlines, groupsH)
+lapply(spl, colMeans)
+
+
+# kmeans
+set.seed(88)
+kmc = kmeans(airlines, centers=5, iter.max=1000)
+table(kmc$cluster)
+spl2 = split(airlines, kmc$cluster)
+lapply(spl2, colMeans)
+table(groupsH, kmc$cluster)
