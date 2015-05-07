0108d9f0b46cdf9bad4cde50c4d2b0005e7c6799
Hojoon Ji <sleeperbus@gmail.com>
2015-05-07 13:02:47 +0900

    ..

--- kaggle/2nd_try.R
+++ kaggle/2nd_try.R
@@ -1,7 +1,27 @@
-news = read.csv("NYTimesBlogTrain.csv", stringsAsFactors=F)
-news$PubDate = strptime(news$PubDate, "%Y-%m-%d %H:%M:%S")
-news$WeekDay = news$PubDate$wday
-news$Hour = news$PubDate$hour
+library(tm)
+library(caTools)
+library(rpart)
+library(rpart.plot)
 
-table(news$Popular, news$WeekDay)
-table(news$Popular, news$Hour)
+news = read.csv("NYTimesBlogTrain.csv", stringsAsFactors=F)
+corpusHeadline = Corpus(VectorSource(news$Headline))
+corpusHeadline = tm_map(corpusHeadline, tolower)
+corpusHeadline = tm_map(corpusHeadline, PlainTextDocument)
+corpusHeadline = tm_map(corpusHeadline, removePunctuation)
+corpusHeadline = tm_map(corpusHeadline, removeWords, stopwords("english"))
+corpusHeadline = tm_map(corpusHeadline, stemDocument)
+dtmHeadline = DocumentTermMatrix(corpusHeadline)
+sparseHeadline = removeSparseTerms(dtmHeadline, 0.99)
+
+headlineWords = as.data.frame(as.matrix(sparseHeadline))
+colnames(headlineWords) = make.names(colnames(headlineWords))
+colMeans(headlineWords)
+headlineWords$Popular = news$Popular
+
+spl = sample.split(headlineWords$Popular, SplitRatio=0.7)
+train = subset(headlineWords, spl==T)
+test = subset(headlineWords, spl==F)
+
+# CART 
+cart.mod = rpart(Popular ~., data=train)
+prp(cart.mod)
