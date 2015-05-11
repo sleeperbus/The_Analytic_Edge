769054f39f3e5373a5bb86e694fef92c5bca505b
Hojoon Ji <sleeperbus@gmail.com>
2015-05-11 12:49:19 +0900

    ..

--- kaggle/2nd_try.R
+++ kaggle/2nd_try.R
@@ -6,7 +6,6 @@ library(SnowballC)
 
 news = read.csv("NYTimesBlogTrain.csv", stringsAsFactors=F)   
 news$PubDate = strptime(news$PubDate, "%Y-%m-%d %H:%M:%S")
-
 news$Weekday = news$PubDate$wday
     
 corpusControls = list(tolower=T, removePunctuation=T, 
@@ -17,42 +16,53 @@ corpusControls = list(tolower=T, removePunctuation=T,
 corpusHeadline = Corpus(VectorSource(news$Headline))
 dtmHeadline = DocumentTermMatrix(corpusHeadline, corpusControls)
 sparseHeadline = removeSparseTerms(dtmHeadline, 0.99)
-headlineWords = as.data.frame(as.matrix(sparseHeadline))
-colnames(headlineWords) = make.names(colnames(headlineWords))
+headline = as.data.frame(as.matrix(sparseHeadline))
+colnames(headline) = make.names(colnames(headline))
 
 # Snippet
 corpusSnippet= Corpus(VectorSource(news$Snippet))
 dtmSnippet= DocumentTermMatrix(corpusSnippet, corpusControls)
 sparseSnippet= removeSparseTerms(dtmSnippet, 0.99)
-snippetWords = as.data.frame(as.matrix(sparseSnippet))
-colnames(snippetWords) = make.names(colnames(snippetWords))
+snippet = as.data.frame(as.matrix(sparseSnippet))
+colnames(snippet) = make.names(colnames(snippet))
 
 # Abstract
 corpusAbstract= Corpus(VectorSource(news$Abstract))
 dtmAbstract= DocumentTermMatrix(corpusAbstract, corpusControls)
 sparseAbstract= removeSparseTerms(dtmAbstract, 0.99)
-abstractWords = as.data.frame(as.matrix(sparseAbstract))
-colnames(abstractWords) = make.names(colnames(abstractWords))
+abstract = as.data.frame(as.matrix(sparseAbstract))
+colnames(abstract) = make.names(colnames(abstract))
+
+headline$Popular = news$Popular
+headlinePopular = subset(headline, Popular==1)
+headlineUnpopular = subset(headline, Popular==0)
+sort(colSums(headlinePopular))
+sort(colSums(headlineUnpopular))
+
+
 
 # merge text variables
-docs = cbind(headlineWords, snippetWords, abstractWords)
+# docs = cbind(headline, snippet, abstract)
+# 
+# docs$Popular = news$Popular
+# docs$NewsDesk = news$NewsDesk
+# docs$SectionName = news$SectionName
+# docs$SubsectionName = news$SubsectionName
+# docs$Weekday = news$Weekday
+# 
+# # exploratory data
+# names(docs)
+# docsPopular = subset(docs, Popular==1)
+# docsUnpopular = subset(docs, Popular==0)
+# 
+# tail(sort(colSums(docsUnpopular[1:470])), 50)
+# tail(sort(colSums(docsPopular[1:470])), 50)
 
-docs$Popular = news$Popular
-docs$NewsDesk = news$NewsDesk
-docs$SectionName = news$SectionName
-docs$SubsectionName = news$SubsectionName
-docs$Weekday = news$Weekday
-
-#corpusHeadline = tm_map(corpusHeadline, tolower)
-#corpusHeadline = tm_map(corpusHeadline, PlainTextDocument)
-#corpusHeadline = tm_map(corpusHeadline, removePunctuation)
-#corpusHeadline = tm_map(corpusHeadline, removeWords, stopwords("english"))
-#corpusHeadline = tm_map(corpusHeadline, stemDocument)
-#dtmHeadline = DocumentTermMatrix(corpusHeadline)
-
+# Set model
 spl = sample.split(docs$Popular, SplitRatio=0.7)
 train = subset(docs, spl==T)
 test = subset(docs, spl==F)
+
 
 # CART 
 cart.mod = rpart(Popular ~., data=train)
