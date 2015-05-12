bab6e1518dd351a7a15de718e280f0f47722fbe2
Hojoon Ji <sleeperbus@gmail.com>
2015-05-12 20:08:08 +0900

    ..

--- Unit7/case_mvt.R
+++ Unit7/case_mvt.R
@@ -30,3 +30,45 @@ ggplot(dayHourCount, aes(x=Hour, y=Day)) +
     geom_tile(aes(fill=Freq)) +
     scale_fill_gradient(name="Total MV Thefts", low="white", high="red") +
     theme(axis.title.y=element_blank())
+
+library(maps)
+library(ggmap)
+
+chicago = get_map(location="chicago", zoom=11)
+ggmap(chicago)
+geoData = as.data.frame(table(round(mvt$Longitude, 2), round(mvt$Latitude,2)))
+str(geoData)
+geoData$Var1 = as.numeric(as.character(geoData$Var1))
+geoData$Var2 = as.numeric(as.character(geoData$Var2))
+colnames(geoData) = c("Long", "Lat", "Freq")
+
+ggmap(chicago) + 
+    geom_point(data=geoData, aes(x=Long, y=Lat, color=Freq, size=Freq)) +
+    scale_colour_gradient(low="yellow", high="red") +
+    theme(axis.title.y = element_blank(), axis.title.x = element_blank())
+
+
+ggmap(chicago) + 
+    geom_tile(data=geoData, aes(x=Long, y=Lat, alpha=Freq), fill="red")
+
+
+murders = read.csv("murders.csv")
+str(murders)
+statesMap = map_data("state")
+class(statesMap)
+str(statesMap)
+ggplot(data=statesMap, aes(x=long,y=lat)) + geom_point()
+murders$region= tolower(murders$State)
+ggplot(data=statesMap, aes(x=long,y=lat,group=group)) + 
+    geom_polygon(fill="white", color="black")
+murderMap = merge(statesMap, murders, by="region")
+str(murderMap)
+
+ggplot(data=murderMap, aes(x=long, y=lat, group=group, fill=Population)) +
+    geom_polygon(color="black") +
+    scale_fill_gradient(low="yellow", high="red", guide="legend")
+
+murderMap$MurderRate = murderMap$Murders / murderMap$Population * 10000
+ggplot(data=murderMap, aes(x=long, y=lat, group=group, fill=MurderRate)) +
+    geom_polygon(color="black", size=0.5) + 
+    scale_fill_gradient(low="black", high="red", limits=c(0,1))
