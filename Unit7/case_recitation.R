5d72c23cbea3a50f1514b592fccb8c300dc7bcf2
Hojoon Ji <sleeperbus@gmail.com>
2015-05-13 21:19:16 +0900

    ..

--- Unit7/case_recitation.R
+++ Unit7/case_recitation.R
@@ -16,3 +16,24 @@ ggplot(data=intl, aes(x=Region, y=PercentOfIntl)) +
     geom_text(aes(label=PercentOfIntl), vjust=-0.4) +
     ylab("Percent of International Students") + 
     theme(axis.title.x = element_blank(), axis.text.x = element_text(angle=30, hjust=1)) 
+
+intlall = read.csv("intlall.csv", stringsAsFactors=F)
+str(intlall)
+head(intlall)
+tail(intlall)
+mapData = map_data("world")
+str(mapData)
+ggplot(mapData, aes(x=long, y=lat, group=group)) + 
+    geom_polygon(fill="white", color="black")
+geoData = merge(mapData, intlall, by.x="region", by.y="Citizenship")
+str(geoData)
+
+ggplot(geoData, aes(x=long, y=lat, group=group)) + 
+    geom_polygon(fill="white", color="black") + 
+    coord_map("mercator")
+head(geoData)
+
+geoData = geoData[with(geoData, order(group, order)), ]
+ggplot(geoData, aes(x=long, y=lat, group=group)) + 
+    geom_polygon(fill="white", color="black") + 
+    coord_map("mercator")
