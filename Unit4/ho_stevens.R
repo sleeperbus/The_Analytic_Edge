2be7a09aeb780625aca7360ea9329eb650788a2d
Hojoon Ji <sleeperbus@gmail.com>
2015-12-07 20:09:44 +0000

    ...

--- Unit4/ho_stevens.R
+++ Unit4/ho_stevens.R
@@ -8,7 +8,7 @@ baseline = 309/nrow(stevens)
 
 # 데이터를 나누자.
 library(caTools)
-set.seed(332)
+set.seed(3000)
 split = sample.split(stevens$Reverse, SplitRatio = 0.7)
 train = subset(stevens, split == TRUE)
 test = subset(stevens, split == FALSE)
@@ -24,9 +24,9 @@ modLog2 = glm(form2, data=train, family=binomial)
 summary(modLog2)
 # AIC = 505.02 로 조금 줄었다. 그렇다면 이걸 사용해서 test set 을 검증하자.
 predTestLog = predict(modLog2, newdata=test, type="response")
-table(test$Reverse, predTestLog > 0.5)
-accuracyLog = (45+65)/(45+32+28+65)
-# logistic model 의 정확도는 0.64 정도로 baseline 보다는 낫다. 
+tblLog = table(test$Reverse, predTestLog > 0.5)
+accuracyLog = (tblLog[1, 1] + tblLog[2, 2])/sum(tblLog)
+# logistic model 의 정확도는 0.69 정도로 baseline 보다는 낫다. 
 
 # 이제 CART 모델을 사용해보자. 
 library(rpart)
@@ -37,8 +37,8 @@ summary(modCART)
 prp(modCART)
 # CART 를 사용해서 결과값을 예측해봅시다.
 predTestCART = predict(modCART, newdata=test, type="class")
-table(test$Reverse, predTestCART)
-accuracyCART = (40+78)/(40+37+15+78)
+tblCART = table(test$Reverse, predTestCART)
+accuracyCART = (tblCART[1, 1] + tblCART[2, 2])/sum(tblCART)
 # higher than log model
 library(ROCR)
 predROC_CART = predict(modCART, newdata=test)
