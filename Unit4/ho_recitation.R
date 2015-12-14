ce2e9664864113e38ece2efb0641526ced26100b
sleeperbus <sleeperbus@gmail.com>
2015-12-15 05:52:54 +0900

    ...

--- Unit4/ho_recitation.R
+++ Unit4/ho_recitation.R
@@ -22,19 +22,67 @@ boxplot(log(boston$CRIM))
 hist(boston$CRIM, breaks=40)
 hist(log(boston$CRIM), breaks=40)
 
-# 데이터 분리
+
+# linear model 
+modLin = lm(MEDV ~ ., data=boston)
+summary(modLin)
+plot(boston$LON, boston$LAT)
+points(boston$LON[boston$MEDV >= 21.2], boston$LAT[boston$MEDV >= 21.2], col="red", pch=20)
+points(boston$LON[modLin$fitted.values >= 21.2], boston$LAT[modLin$fitted.values >= 21.2], col="blue", pch="$")
+
+modLin2 = lm(MEDV ~ . - TOWN - TRACT, data=boston)
+summary(modLin2)
+plot(boston$LON, boston$LAT)
+points(boston$LON[boston$MEDV >= 21.2], boston$LAT[boston$MEDV >= 21.2], col="red", pch=20)
+points(boston$LON[modLin2$fitted.values >= 21.2], boston$LAT[modLin2$fitted.values >= 21.2], col="blue", pch="$")
+
+library(rpart)
+library(rpart.plot)
+modCART = rpart(MEDV ~ LAT + LON, data=boston)
+fittedCART = predict(modCART)
+prp(modCART)
+plot(boston$LON, boston$LAT)
+points(boston$LON[boston$MEDV >= 21.2], boston$LAT[boston$MEDV >= 21.2], col="red", pch=20)
+points(boston$LON[fittedCART >= 21.2], boston$LAT[fittedCART >= 21.2], col="blue", pch="$")
+
+# simplify
+modCART2 = rpart(MEDV ~ LAT + LON, data=boston, minbucket=50)
+plot(modCART2)
+text(modCART2)
+
 library(caTools)
-set.seed(8)
+set.seed(123)
 split = sample.split(boston$MEDV, SplitRatio = 0.7)
 train = subset(boston, split == TRUE)
 test = subset(boston, split == FALSE)
 
-# linear model 
-modLin1 = lm(MEDV ~ . - TRACT, data=train)
-summary(modLin1)
+# linear model
+modLin = lm(MEDV ~ LON + LAT + CRIM + ZN + INDUS + CHAS + NOX + RM + AGE + DIS + RAD + TAX + PTRATIO, data=train)
+summary(modLin)
+predLin = predict(modLin, newdata=test)
+sseLin = sum((test$MEDV - predLin)^2)
 
+# CART model
+modCART = rpart(MEDV ~ LON + LAT + CRIM + ZN + INDUS + CHAS + NOX + RM + AGE + DIS + RAD + TAX + PTRATIO, data=train)
+prp(modCART)
+summary(modCART)
+predCART = predict(modCART, newdata=test)
+sseCART = sum((test$MEDV - predCART)^2)
 
+# randomForest 
+library(randomForest)
+modRF = randomForest(MEDV ~ LON + LAT + CRIM + ZN + INDUS + CHAS + NOX + RM + AGE + DIS + RAD + TAX + PTRATIO, data=train)
+print(modRF)
+summary(modRF)
+predRF = predict(modRF, newdata=test)
+sseRF = sum((test$MEDV - predRF)^2)
 
-
-
-predTest = predict(modLin1, newdata=test)
+library(caret)
+library(e1071)
+trCtrl = trainControl(method="cv", number=10)
+cpGrid = expand.grid(.cp=(0:10)*0.001) 
+tr = train(MEDV ~ LON + LAT + CRIM + ZN + INDUS + CHAS + NOX + RM + AGE + DIS + RAD + TAX + PTRATIO, data=train,
+           method="rpart", trControl=trCtrl, tuneGrid=cpGrid)
+prp(tr$finalModel)
+predCV = predict(tr$finalModel, newdata=test)
+sseCV = sum((test$MEDV - predCV)^2)
