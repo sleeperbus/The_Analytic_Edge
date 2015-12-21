b6541c604ccac127b21e35828e4d5882409449f0
sleeperbus <sleeperbus@gmail.com>
2015-12-22 05:04:40 +0900

    ..

--- Unit6/ho_recitation.R
+++ Unit6/ho_recitation.R
@@ -21,7 +21,7 @@ str(healthyMat)
 image(healthyMat, axes=FALSE, col=grey(seq(0,1,length=256)))
 healthyVec = as.vector(healthyMat)
 str(healthyVec)
-distHealthy = dist(healthyVec)
+#distHealthy = dist(healthyVec)
 # 각 pixel 간의 거리를 구하기에는 너무 멀다. 
 
 # kmeans 
@@ -33,6 +33,24 @@ dim(healthyMat)
 resultMat = km$cluster
 dim(resultMat) = dim(healthyMat)
 image(resultMat, axes=FALSE, col=grey(seq(0,1,length=5)))
+km$withinss
 
+# cluster 개수 정하기
+numClusters = seq(2,10,1)
+sumWithinss = sapply(numClusters, function(x) {sum(kmeans(healthyVec, centers=x, iter.max=1000)$withinss)})
+plot(numClusters, sumWithinss, type="o")
 
-
+# 이 결과를 적용 
+tumor = read.csv("tumor.csv", header=FALSE)
+str(tumor)
+tumorVect = as.vector(as.matrix(tumor))
+str(tumorVect)
+library(flexclust)
+tumorKCCA = as.kcca(km, healthyVec)
+tumorPred = predict(tumorKCCA, newdata=tumorVect)
+summary(tumorPred)
+unique(tumorPred)
+tumorMat = tumorPred
+dim(tumorMat) = dim(as.matrix(tumor))
+image(tumorMat, axes=FALSE, col=grey(seq(0,1,length=5)))
+image(as.matrix(tumor), axes=FALSE, col=grey(seq(0,1,length=256)))
